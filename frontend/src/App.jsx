f0d1344400078509ac85d3897011d00b89697acb
Sinan <muhammedsinan7749@gmail.com>
2024-06-27 12:14:46 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,3 +19,53 @@ const App = () => {
 };
 
 export default App;
+
+
+
+// ! With profiler for developing
+
+// import React, { Profiler } from "react";
+// import Header from "./components/Header.jsx";
+// import { Outlet } from "react-router-dom";
+// import { Container } from "react-bootstrap";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import LoadingProvider from "./provider/IsLoadingProvider.jsx";
+
+// const onRenderCallback = (
+//   id,
+//   phase,
+//   actualDuration,
+//   baseDuration,
+//   startTime,
+//   commitTime,
+//   interactions
+// ) => {
+//   console.log({
+//     id,
+//     phase,
+//     actualDuration,
+//     baseDuration,
+//     startTime,
+//     commitTime,
+//     interactions
+//   });
+// };
+
+// const App = () => {
+//   return (
+//     <LoadingProvider>
+//       <Profiler id="Header" onRender={onRenderCallback}>
+//         <Header />
+//       </Profiler>
+//       <ToastContainer />
+//       <Container className="my-2">
+//         <Profiler id="Outlet" onRender={onRenderCallback}>
+//           <Outlet />
+//         </Profiler>
+//       </Container>
+//     </LoadingProvider>
+//   );
+// };
+
+// export default App;
